1448c663941ef93ea39dce930069262b3bba0db2
Fernando200114 <fernandojosellullunacuichan@gmail.com>
2025-07-13 15:35:36 -0500

    rectificacion main

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 🚨 Habilitar CORS para permitir peticiones desde el frontend
+  app.enableCors({
+    origin: ['http://localhost:5173'], // frontend local de React (Vite)
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3013);
 }
 bootstrap();
